975547358c715dc4b21c7c18a2efada400535427
wuqingtao <wqt.acc@gmail.com>
2017-10-27 21:09:27 +0800

    change cron to cr

--- cron_test.go
+++ cron_test.go
@@ -13,41 +13,41 @@ func TestCron(t *testing.T) {
 	s3 := "1/5 * * * * *"
 	s4 := "8/12 * * * * *"
 	ctx, cancel := context.WithCancel(context.Background())
-	cron := New(ctx, cancel)
+	cr := New(ctx, cancel)
 
 	go func() {
-		for err := range cron.Err {
+		for err := range cr.Err {
 			fmt.Printf("ERROR: %s\n", err)
 		}
 	}()
 
 	fmt.Printf("%s\n", time.Now())
 	fmt.Println("add s1")
-	cron.AddFunc(ctx, "s1", s1, func() {
+	cr.AddFunc(ctx, "s1", s1, func() {
 		fmt.Printf("s1 %s: %s\n", s1, time.Now())
 	})
-	go cron.Start(ctx)
+	go cr.Start(ctx)
 	fmt.Println("add s2")
-	cron.AddFunc(ctx, "s2", s2, func() {
+	cr.AddFunc(ctx, "s2", s2, func() {
 		fmt.Printf("s2 %s: %s\n", s2, time.Now())
 	})
 	fmt.Println("add s3")
-	cron.AddFunc(ctx, "s3", s3, func() {
+	cr.AddFunc(ctx, "s3", s3, func() {
 		fmt.Printf("s3 %s: %s\n", s3, time.Now())
 	})
 	fmt.Println("add s4, but name is s3")
-	cron.AddFunc(ctx, "s3", s4, func() {
+	cr.AddFunc(ctx, "s3", s4, func() {
 		fmt.Printf("s4 %s: %s\n", s4, time.Now())
 	})
 
 	go func() {
 		time.Sleep(5 * time.Second)
 		fmt.Println("delete s1")
-		cron.Delete("s1")
+		cr.Delete("s1")
 	}()
 	time.Sleep(10 * time.Second)
 	fmt.Println("stop cron")
-	cron.Stop()
+	cr.Stop()
 	fmt.Println("wait stop!")
 	time.Sleep(1 * time.Second)
 }
